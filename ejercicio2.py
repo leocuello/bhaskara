cd673dc8c5d633320c29aa03a801c938c61e0ed9
leocuello@gmail.com <leocuello@gmail.com>
2021-11-16 11:41:00 -0300

    Primer commit

--- ejercicio2.py
+++ ejercicio2.py
@@ -10,7 +10,7 @@ if raiz < 0:
     print("No tiene solucion en R, es un complejo")
 elif raiz == 0:
     x1 = (-b) / (2*a)
-    print(x1) 
+    print("X1: ", x1) 
 else:
     x1 = (-b + (raiz ** 0.5)) / (2*a)
     x2 = (-b - (raiz ** 0.5)) / (2*a)
