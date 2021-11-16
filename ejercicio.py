c168b4940552508981a968048d64d4d3db33d371
Leo <leocuello@gmail.com>
2021-11-16 12:14:48 -0300

    Update ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,8 +1,8 @@
-#1) Dar informacion preceisa al usuario, cambiar primer, segunto, tercer numero por "la variable cuadrática", "variable lineal", "término independiente"
+#1) Dar informacion precisa al usuario, cambiar primer, segundo, tercer numero por "la variable cuadrática", "variable lineal", "término independiente"
 a = int(input("Introduce el primer número: "))
 b = int(input("Introduce el segundo número: "))
 c = int(input("Introduce el tercer número: "))
-#2) Validar que sean numeros enteros (Creo que con WHILE)
+#2) Validar que sean números enteros (Creo que con WHILE)
 
 
 #3) Validar que lo que esta DEBAJO de la RAIZ sea mayor a "0",  (b ** 2 - 4 * (a * c))
@@ -11,7 +11,7 @@ c = int(input("Introduce el tercer número: "))
 
 #4) Validar si la RAIZ = 0 
 
-#5) Veo 2 blqoes parecidos, para calcualr X e Y, no es claro
+#5) Veo 2 bloqoes parecidos, para calcular X e Y, no es claro
 
 x = b ** 2 - 4 * (a * c)
 x = x ** 0.5
@@ -23,7 +23,7 @@ y = y ** 0.5
 y = - b - y
 y = y / (2 * a)
  
-#6) Usar nombre de variables represetativas y claras
+#6) Usar nombre de variables representativas y claras
 # X e Y deben ser X1 y X2
 
 #7) Evitar acumular resultados en la misma variable
